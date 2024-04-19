b726cd0c3926fa714b1810a31fc75e85e085d704
Amit Kumar <amit2005tp@gmail.com>
2024-04-19 19:38:17 +0530

    create get posts api

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import Post from "../models/postModel.js";
 export const create = async (req, res, next) => {
-//   console.log(req.user);
+  //   console.log(req.user);
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
   }
@@ -24,5 +24,50 @@ export const create = async (req, res, next) => {
     res.status(201).json(savedPost);
   } catch (error) {
     next(error);
+  }
+};
+export const getposts = async (req, res, next) => {
+//   console.log("called getposts");
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const filter = {
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    };
+
+    const posts = await Post.find(filter)
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    console.log("error");
   }
 };
